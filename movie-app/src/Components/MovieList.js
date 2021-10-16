e26777c40e82a28358f1bb654a4b30d56762febc
ambuj0106 <ambujverma00@gmail.com>
2021-10-16 17:11:08 +0530

    pagination and Api done

--- movie-app/src/Components/MovieList.js
+++ movie-app/src/Components/MovieList.js
@@ -1,8 +1,71 @@
 import React, { Component } from "react";
-import { getMovies } from "./getMovie";
+import axios from "axios";
 export default class MovieList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hover: "",
+      parr: [1],
+      currPage: 1,
+      movie: [],
+    };
+  }
+  async componentDidMount() {
+    // Side effects
+    const res =
+      await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=c31c674c6542b694c057197919f2d628&language=en-US&page=${this.state.currPage}
+    `);
+    let popMovies = res.data;
+    this.setState({
+      movie: [...popMovies.results],
+    });
+    console.log(res);
+    console.log("mounting Done");
+  }
+  changeMovies = async () => {
+    const res =
+      await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=c31c674c6542b694c057197919f2d628&language=en-US&page=${this.state.currPage}
+    `);
+    let popMovies = res.data;
+    this.setState({
+      movie: [...popMovies.results],
+    });
+  };
+  handleNext = () => {
+    let tmpArr = [];
+    for (let i = 1; i <= this.state.parr.length + 1; i++) {
+      tmpArr.push(i);
+    }
+    this.setState(
+      {
+        currPage: this.state.currPage + 1,
+        parr: [...tmpArr],
+      },
+      this.changeMovies
+    );
+  };
+  handlePrev = () => {
+    if (this.state.currPage != 1) {
+      this.setState(
+        {
+          currPage: this.state.currPage - 1,
+        },
+        this.changeMovies
+      );
+    }
+  };
+  handleNum = (pn) => {
+    if (pn != this.state.currPage) {
+      this.setState(
+        {
+          currPage: pn,
+        },
+        this.changeMovies
+      );
+    }
+  };
   render() {
-    let movies = getMovies.results;
+    let movies = this.state.movie;
     return (
       <>
         {movies.length == 0 ? (
@@ -16,7 +79,11 @@ export default class MovieList extends Component {
             <h3 className="text-center">Trending</h3>
             <div className="movie-list">
               {movies.map((movieObj) => (
-                <div className="card movie-card">
+                <div
+                  className="card movie-card"
+                  onMouseEnter={() => this.setState({ hover: movieObj.id })}
+                  onMouseLeave={() => this.setState({ hover: "" })}
+                >
                   <img
                     src={`https://image.tmdb.org/t/p/w400/${movieObj.backdrop_path}`}
                     className="card-img-top movie-img"
@@ -37,13 +104,41 @@ export default class MovieList extends Component {
                       justifyContent: "center",
                     }}
                   >
-                    <a href="#" className="btn btn-primary movie-button">
-                      Add to favourite
-                    </a>
+                    {this.state.hover == movieObj.id && (
+                      <a href="#" className="btn btn-primary movie-button">
+                        Add to favourite
+                      </a>
+                    )}
                   </div>
                   {/* </div> */}
                 </div>
               ))}
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <nav aria-label="Page navigation example">
+                <ul className="pagination">
+                  <li className="page-item">
+                    <a className="page-link" onClick={this.handlePrev}>
+                      Previous
+                    </a>
+                  </li>
+                  {this.state.parr.map((pn) => (
+                    <li className="page-item">
+                      <a
+                        className="page-link"
+                        onClick={() => this.handleNum(pn)}
+                      >
+                        {pn}
+                      </a>
+                    </li>
+                  ))}
+                  <li className="page-item">
+                    <a className="page-link" onClick={this.handleNext}>
+                      Next
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </div>
           </div>
         )}
